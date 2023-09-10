b114af4ba346e46ce8c1964b52924930f887cfa9
Deepak kumar <dkranchii@gmail.com>
2023-09-10 11:15:49 -0700

    Create reverseListRecursive.py
    Reverse the list based on Recursion

---
+++ ds/reverseListRecursive.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # solution based on recursion
+        #define the base case
+        if (not head) or (not head.next):
+            return head
+
+        p = self.reverseList(head.next)
+        head.next.next= head
+        head.next = None
+        return p
+
+
+#Time complexity : O(n)
+#Space complexity : O(1)
